f8f10c43e1b89aeedc9f3d136c8089dd12cddfc2
Theron Boerner <hunterboerner@gmail.com>
2013-02-11 20:32:29 -0600

    Fix derp

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+New::Application.config.secret_token = '7f8d6b572cbde4a05742c54ca172b88db2fd8c23affd0e368bad0e6fb344f67910ae7ab2515ff16c02da8e29d7965ea2e8a36832a702df785c7c2e8404a82078'
